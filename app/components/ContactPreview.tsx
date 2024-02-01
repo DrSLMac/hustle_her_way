fcd29f98194ababee26b131133780a1c32bbae92
Shauna MacFarlane-Okongo <shauna.macfarlane579@gmail.com>
2024-02-01 14:59:58 -0600

    replaced apostrophe

--- app/components/ContactPreview.tsx
+++ app/components/ContactPreview.tsx
@@ -5,7 +5,7 @@ const ContactPreview = () => {
     <div>
       <div>
         <h1>Contact Us</h1>
-        <p>Have questions? Concerns? Suggestions? I'm here for it all.</p>
+        <p>Have questions? Concerns? Suggestions? I&apos;m here for it all.</p>
         <br />
         <p>Feel free to reach out.</p>
         <br />
@@ -17,7 +17,7 @@ const ContactPreview = () => {
       <div>
         <h2>Stay in the know</h2>
         <h2>Join the mailing list</h2>
-        
+
       </div>
     </div>
   );
