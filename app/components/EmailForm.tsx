592e200ae9d5e7c61e79b3188f5de175f96e7d9e
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2024-02-12 16:14:44 -0600

    edits to form

--- app/components/EmailForm.tsx
+++ app/components/EmailForm.tsx
@@ -6,40 +6,14 @@ import { ToastAction } from "@/components/ui/toast";
 
 const EmailForm = () => {
   const [emailInput, setEmailInput] = useState("");
-  const [message, setMessage] = useState("");
+  const [nameInput, setNameInput] = useState("");
   const { toast } = useToast();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("this has been clicked");
-    try {
-      // const response = await axios.post('/api/subscribe', { emailInput });
-      const response = await axios.post(
-        `${window.location.origin}/api/subscribe`,
-        { emailInput }
-      );
-      setMessage(response.data.message);
-      setEmailInput("");
-      toast({
-        title: "You have successfully subscribed!",
-        // description: (error),
-        action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
-      });
-    } catch (error) {
-      setMessage("Failed to subscribe email!");
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        // description: (error),
-        action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
-      });
-      console.error("Error subscribing email:", error);
-    }
-  };
+ console.log("emailInput: ", emailInput)
+
 
   return (
     <form
-      // onSubmit={handleSubmit}
       className="flex flex-col justify-center space-y-4 my-4 rounded-md"
       method="post" 
       action="https://systeme.io/embedded/14686218/subscription"
@@ -49,26 +23,64 @@ const EmailForm = () => {
         name="first_name" 
         placeholder="Enter your first name"
         className="rounded-md text-sm mobile:text-base w-full p-3"
+        required
+        value={nameInput}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setNameInput(e.target.value)
+        }
       />
        <input
-        type="text"
+        type="email"
+        required
         name="email"
         placeholder="Enter your email address"
-        // value={emailInput}
-        // onChange={(e: ChangeEvent<HTMLInputElement>) =>
-        //   setEmailInput(e.target.value)
-        // }
+        value={emailInput}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setEmailInput(e.target.value)
+        }
         className="rounded-md text-sm mobile:text-base w-full p-3"
       />
+
     <Button
         type="submit"
         className="mx-10 p-2 rounded-lg bg-secondary text-accent uppercase ease-in-out duration-300"
       >
-        Sign up to receive emails
+        click the button
       </Button>
+
+
     </form>
   
   );
 };
 
 export default EmailForm;
+
+
+ // const handleSubmit = async (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   console.log("this has been clicked");
+  //   try {
+  //     // const response = await axios.post('/api/subscribe', { emailInput });
+  //     const response = await axios.post(
+  //       `${window.location.origin}/api/subscribe`,
+  //       { emailInput }
+  //     );
+  //     setMessage(response.data.message);
+  //     setEmailInput("");
+  //     toast({
+  //       title: "You have successfully subscribed!",
+  //       // description: (error),
+  //       action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
+  //     });
+  //   } catch (error) {
+  //     setMessage("Failed to subscribe email!");
+  //     toast({
+  //       variant: "destructive",
+  //       title: "Uh oh! Something went wrong.",
+  //       // description: (error),
+  //       action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
+  //     });
+  //     console.error("Error subscribing email:", error);
+  //   }
+  // };
